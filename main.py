9e95841a080382c214c31bc91426968ee0479ee6
Andrée Ekroth <andree.ekroth@gmail.com>
2016-05-17 23:08:59 +0200

    main: quick and dirty str parsing

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
-
 import numpy as np
-
+import re
 from xkcd_plot import XKCDify
-
 import telegram
 from telegram.error import NetworkError, Unauthorized
 
@@ -39,44 +37,78 @@ def main():
 def handle(bot, update_id):
 
     updates = bot.getUpdates(offset=update_id, timeout=10)
-    print([u.message.text for u in updates])
+
 
     for u in updates:
         update_id = u.update_id + 1
 
         chat_id = u.message.chat_id
+        text = u.message.text.replace('/plottybot ', '')
+        print("received input " + text)
+        try:
+            func = string2func(text)
+            a = -3.14
+            b = 3.14
+            x = np.linspace(a, b, 250)
+            ax = plt.axes()
+            ax.plot(x, func(x))
+        except ValueError, e:
+            bot.sendMessage(chat_id=chat_id, text="Invalid input" + str(e))
+            continue
+        except SyntaxError, e:
+            bot.sendMessage(chat_id=chat_id, text="Invalid input" + str(e))
+            continue
 
-        bot.sendMessage(chat_id=chat_id, text="Andy is king you know")
 
-        ax = test_fig()
+        plt.xlim(a, b)
+
+        XKCDify(ax,
+                xaxis_loc=0.0,
+                yaxis_loc=1.0,
+                xaxis_arrow='+-',
+                yaxis_arrow='+-',
+                expand_axes=True)
+
         plt.savefig('temp.png')
-
         bot.sendPhoto(chat_id=chat_id, photo=open('temp.png', 'rb'))
         plt.clf()
 
     return update_id
 
-def test_fig():
-    ax = plt.axes()
+# Thank you, MaxNoe.
+# http://stackoverflow.com/questions/32726992/how-to-plot-a-math-function-from-string
+replacements = {
+    'sin' : 'np.sin',
+    'cos' : 'np.cos',
+    'exp': 'np.exp',
+    'sqrt': 'np.sqrt',
+    '^': '**',
+}
 
-    x = np.linspace(-10, 10, 100)
-    ax.plot(x, -x, 'b')
+allowed_words = [
+    'x',
+    'sin',
+    'cos',
+    'sqrt',
+    'exp',
+]
 
-    ax.set_title('APPLE IS KING')
-    ax.set_xlabel('IPHONES')
-    ax.set_ylabel('BROKEN')
+def string2func(string):
+    ''' evaluates the string and returns a function of x '''
+    # find all words and check if all are allowed:
+    for word in re.findall('[a-zA-Z_]+', string):
+        if word not in allowed_words:
+            raise ValueError(
+                '"{}" is forbidden to use in math expression'.format(word)
+            )
 
-    ax.legend(loc='lower right')
+    for old, new in replacements.items():
+        string = string.replace(old, new)
 
-    XKCDify(ax,
-            xaxis_loc=0.0,
-            yaxis_loc=0.0,
-            xaxis_arrow='+-',
-            yaxis_arrow='+-',
-            expand_axes=True)
+    def func(x):
+        return eval(string)
 
-    return ax
-
+    return func
 
 if __name__ == "__main__":
     main()
