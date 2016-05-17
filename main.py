9e5c45e719468023c466c2ad133a143f7982126a
Andrée Ekroth <andree.ekroth@gmail.com>
2016-05-17 15:14:16 +0200

    main: read apikey

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import telegram
 
 def main():
-    bot = telegram.Bot(token='token')
+    token='token'
+    with open('.apikey', 'r') as fil:
+        token = fil.read().replace('\n', '')
+    bot = telegram.Bot(token=token)
     print(bot.getMe())
 
 if __name__ == "__main__":
